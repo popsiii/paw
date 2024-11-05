2ad38386e2e68d044e5580193d379953dbc584c0
Marta <mpopielska1@st.swp0s.edu.pl>
2024-11-05 14:54:39 +0100

    stanowisko z id

--- pawcwiczenia/paw_app/admin.py
+++ pawcwiczenia/paw_app/admin.py
@@ -9,12 +9,23 @@ admin.site.register(Person)
 from django.contrib import admin
 from .models import Osoba, Stanowisko
 
-admin.site.register(Stanowisko)
+class StanowiskoAdmin(admin.ModelAdmin) :
+    list_display = ( "nazwa", "opis" )
+    search_fields = ( " nazwa " , )
+
+admin.site.register(Stanowisko, StanowiskoAdmin)
+
 
 class OsobaAdmin(admin.ModelAdmin):
-    list_display = ("imie","nazwisko","plec","stanowisko","data_dodania")
+    list_display = ("imie","nazwisko","plec","stanowisko_z_id","data_dodania")
     fields = ("imie","nazwisko","plec","stanowisko")
     readonly_fields = ("data_dodania", )
     search_fields = ("imie", "nazwisko")
+    list_filter = ("data_dodania", "stanowisko")
+    
+
+    @admin.display(description= "Stanowisko(id)")
+    def stanowisko_z_id (self,obj) :
+        return f"{obj.stanowisko} ({obj.stanowisko.id})"
 
 admin.site.register(Osoba, OsobaAdmin)
